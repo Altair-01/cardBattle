98ab5d1e9012fb6eada8d273928389999e2da8f7
Altair-01 <omzosonko@gmail.com>
2022-06-03 14:57:13 +0000

    Initial Commit

---
+++ cardBattle/cardBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardBattleApp.swift
+//  cardBattle
+//
+//  Created by Oumar Sonko on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct cardBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
